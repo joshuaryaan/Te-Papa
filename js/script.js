65bcf2df215c5b160a20bc227ad40a654fe29fdc
joshuaryaan <joshua.ryan.warner@icloud.com>
2016-04-13 00:18:40 +1200

    popup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function () { 
+    
+    $(".close").hide();
+    $(".pop").hide();
+    
+$(".item").click(function () {
+    $(".pop").show();
+    $(".pop").animate({"top": "49px"}, "1500");
+    $(".close").fadeIn(400);
+});
+    
+$(".close").click(function () {
+    $(".pop").animate({"top": "100vh"}, "1500");
+    $(".close").fadeOut(200);
+    $(".pop").hide(100);
+});
 
+    
 });
