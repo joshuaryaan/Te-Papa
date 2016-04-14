58b61b3c869f379a5feb2b5bd7b8fb96f87bc34c
joshuaryaan <joshua.ryan.warner@icloud.com>
2016-04-14 23:55:01 +1200

    end of div scroll fixing
    http://jsbin.com/itajok/539/edit?html,js,output

--- js/script.js
+++ js/script.js
@@ -34,4 +34,18 @@ $(".close").click(function () {
     $('.content').unbind('touchmove');
 });
     
+    $(function() {
+
+  var toolbox = $('.pop'),
+      height = toolbox.height(),
+      scrollHeight = toolbox.get(0).scrollHeight;
+
+  toolbox.bind('mousewheel', function(e, d) {
+    if((this.scrollTop === (scrollHeight - height) && d < 0) || (this.scrollTop === 0 && d > 0)) {
+      e.preventDefault();
+    }
+  });
+
+});
+    
 });
