cd7d5237016f8ec69596eaab97960fe4fedfcc7d
joshuaryaan <joshua.ryan.warner@icloud.com>
2016-04-14 23:56:46 +1200

    touchmove

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ $(".close").click(function () {
       height = toolbox.height(),
       scrollHeight = toolbox.get(0).scrollHeight;
 
-  toolbox.bind('mousewheel', function(e, d) {
+  toolbox.bind('touchmove', function(e, d) {
     if((this.scrollTop === (scrollHeight - height) && d < 0) || (this.scrollTop === 0 && d > 0)) {
       e.preventDefault();
     }
